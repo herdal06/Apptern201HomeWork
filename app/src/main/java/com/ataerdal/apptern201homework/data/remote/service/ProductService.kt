2b4c78833f36009e9da402978e73714c65c3432f
herdal06 <h.erdal721@gmail.com>
2023-07-12 14:25:31 +0300

    Update BaseResponse and add getProductDetail method to service

--- app/src/main/java/com/ataerdal/apptern201homework/data/remote/service/ProductService.kt
+++ app/src/main/java/com/ataerdal/apptern201homework/data/remote/service/ProductService.kt
@@ -3,9 +3,15 @@ package com.ataerdal.apptern201homework.data.remote.service
 import com.ataerdal.apptern201homework.base.model.BaseResponse
 import com.ataerdal.apptern201homework.data.remote.dto.product.ProductDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ProductService {
 
     @GET("products")
-    suspend fun getAllProducts(): BaseResponse<ProductDto>?
+    suspend fun getAllProducts(): BaseResponse<List<ProductDto>>?
+
+    @GET("products/{id}")
+    suspend fun getProductDetail(
+        @Path("id") productId: Int
+    ): BaseResponse<ProductDto>?
 }
